0c84dbb3a744e47abc299d12ea2b21eda4cb53c3
Valentino Lugli <valentinolugli@gmail.com>
2021-03-19 23:11:23 +0100

    [P1] Warming up practice 1

---
+++ practica1/COPKM.cpp
@@ -0,0 +1,23 @@
+
+// LIBRARIES
+//      Entrada/Salida
+#include <iostream>
+
+// FUNCTIONS
+void loadData(int argc, char * arg[])
+{
+    
+}
+
+void greed()
+{
+
+}
+
+// k n set.dat set_const.const
+int main(int argc, char * argv[])
+{
+    std::cout<<argv[1]<<argv[2]<<argv[3]<<std::endl;
+    //loadData()
+    return 0;
+}
